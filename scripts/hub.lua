8cf05371bcda78d9670cd0016dc932e047fc73ce
Slavik <160470809+Slavik728@users.noreply.github.com>
2025-02-14 22:44:03 -0300

    i forgot to add the actual holdtointeract action

--- scripts/hub.lua
+++ scripts/hub.lua
@@ -115,7 +115,11 @@ local Keybind = ToolTab:CreateKeybind({
    HoldToInteract = true,
    Flag = "Keybind1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(ABKeybind)
-   ANTI_BANG(true, ABDegree)
+      if ABKeybind then
+         ANTI_BANG(true, ABDegree)
+      else
+         ANTI_BANG(false, ABDegree)
+      end
    end,
 })
 
