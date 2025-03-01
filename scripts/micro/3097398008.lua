c8922b721ffdb9b56ab6c17d66c18cebae61a59a
Slavik <160470809+Slavik728@users.noreply.github.com>
2025-03-01 18:33:00 -0300

    The Glass Bridge

--- scripts/micro/3097398008.lua
+++ scripts/micro/3097398008.lua
@@ -15,17 +15,18 @@ Window:Toggle({
             end
             local function onGlassReset(c)
                 if c:IsA('Model') and c.Name == 'GlassBridges' then
-                    task.wait(15)
+                    task.wait()
                     for i, g in pairs(grs.GlassBridges:WaitForChild('CorrectGlass'):GetChildren()) do
                         g.Transparency = .7
                     end
                 end
             end
+            grs.ChildAdded:Connect(onGlassReset)
         else
+            onGlassReset:Disconnect()
             for i, g in pairs(grs.GlassBridges.CorrectGlass:GetChildren()) do
                 g.Transparency = 1
             end
-        end
-        grs.ChildAdded:Connect(onGlassReset)
+         end
     end
 })
